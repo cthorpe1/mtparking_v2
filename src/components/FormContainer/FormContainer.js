4bab2eb47123b1b6ee2760f191ad3f7a549b7460
cthorpe1 <chasethorpe1@gmail.com>
2019-03-26 10:59:18 -1000

    Changed Keiki Class option to Keiki MT in form

--- src/components/FormContainer/FormContainer.js
+++ src/components/FormContainer/FormContainer.js
@@ -54,7 +54,7 @@ const formContainer = (props) => {
                       <option value="Foundations">Foundations MT</option>
                       <option value="Level 1">Level 1 MT</option>
                       <option value="Level 2">Level 2 MT</option>
-                      <option value="Keiki Class">Keiki MT</option>
+                      <option value="Keiki MT">Keiki MT</option>
                       <option value="TRX/HIIT">TRX/HIIT</option>
                       <option value="Yoga">Yoga</option>
                       <option value="Coaching">Coaching</option>
